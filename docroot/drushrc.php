fa5fd5fb86bf7458d497923dbb08c74279a043e1
Randy Fay <randy@randyfay.com>
2025-12-01 17:40:35 -0700

    Migrate to backdropcms

---
+++ docroot/drushrc.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * #ddev-generated: Automatically generated drushrc.php file (for Drush 8)
+ * DDEV manages this file and may delete or overwrite it unless this comment is removed.
+ * Remove this comment if you don't want DDEV to manage this file.
+ */
+
+if (getenv('IS_DDEV_PROJECT') == 'true') {
+  $options['l'] = "https://randyfay.ddev.site";
+}
